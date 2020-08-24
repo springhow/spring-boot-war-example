1368abc7658b7203f1e2340feab5f5413953e71e
Raja Anbazhagan <raja.anbazhagan1991@gmail.com>
2020-08-24 19:31:59 +0530

    changed version to 2.3.2

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -1,26 +1,29 @@
 package com.springhow.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 @SpringBootApplication
 public class HelloWorldApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(HelloWorldApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class);
+    }
 
 
-	@RequestMapping("/")
-	String helloWorld() {
-		return "Hello World!";
-	}
+    @RequestMapping("/")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
 }
